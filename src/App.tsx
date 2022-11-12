545374704c758aaddb404ba7d34ba91090a379b7
Artem Markelov <artem77timoha77@gmail.com>
2022-11-12 17:25:48 +0600

    created a button, when clicked, a modal window opens, which accepts an array for creating buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Modal from "./components/Modal/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const cancel = () => setShowModal(false);
+  const newAlert = () => alert('New Alert')
+
+
+const BtnConfiguration = [
+  {classBtn: 'btn btn-primary', title: 'Continue', onClickBtn: newAlert},
+  {classBtn: 'btn btn-danger', title: 'Close', onClickBtn: cancel}
+]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex justify-content-around mt-5">
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => setShowModal(true)}
+      >Modal
+      </button>
+
+      <Modal
+        show={showModal}
+        title={'Some kinda modal title'}
+        onClose={cancel}
+        btnConfiguration={BtnConfiguration}
+      >
+        <div className="modal-body">
+          This is modal content
+        </div>
+      </Modal>
     </div>
   );
 }
